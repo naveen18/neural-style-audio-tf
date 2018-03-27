0a6cc59158f3d5225d2a87d4d91cf10a6384f494
Naveen Bansal <naveen18jan93@gmail.com>
2018-03-26 21:19:03 -0400

    calculated total loss

--- neural-style-audio-pt.py
+++ neural-style-audio-pt.py
@@ -36,8 +36,8 @@ a_style_tf = np.transpose(a_style_tf, (0, 3, 1, 2))
 std = np.sqrt(2) * np.sqrt(2.0 / ((N_CHANNELS + N_FILTERS) * 11))
 kernel = np.random.randn(1, 11, N_CHANNELS, N_FILTERS)*std
 
-content_var = Variable(torch.from_numpy(a_content_tf))
-style_var = Variable(torch.from_numpy(a_style_tf))
+content_var = Variable(torch.from_numpy(a_content_tf), requires_grad = False)
+style_var = Variable(torch.from_numpy(a_style_tf), requires_grad = False)
 
 conv = torch.nn.Conv2d(N_CHANNELS, N_FILTERS, kernel_size = (1, 11))
 
@@ -49,31 +49,31 @@ relu = torch.nn.ReLU()
 content_features = relu(content_features_tensor).data.numpy()
 style_features = relu(style_features).data.numpy()
 
-print("after")
-print(content_features.shape)
-print(style_features.shape)
 features = np.reshape(style_features, (-1, N_FILTERS))
 style_gram = np.matmul(features.T, features)/N_SAMPLES
-print(style_gram.shape)
 
 ALPHA= 1e-2
 learning_rate= 1e-3
 iterations = 100
 result = None
 
-
 x = Variable(torch.randn(1, N_CHANNELS, 1, N_SAMPLES).type(torch.FloatTensor)*1e-3, requires_grad = True)
+# x = Variable(torch.from_numpy(a_content_tf), requires_grad = True)
 conv_x = conv(x)
-net_x = relu(x)
+net_x = relu(conv_x)
 loss = torch.nn.MSELoss()
 style_loss = 0
-content_loss = 2 * ALPHA * loss(net_x.data, content_features)
-# _, height, width, number = map(lambda i: i.value, net.get_shape())
-# size = height * width * number
-# feats = tf.reshape(net_x, (-1, number))
-# gram = tf.matmul(tf.transpose(feats), feats)  / N_SAMPLES
-# style_loss = 2 * torch.nn.l2_loss(gram - style_gram)
-# loss = content_loss + style_loss
+target = Variable(torch.FloatTensor(content_features), requires_grad = False)
+content_loss = 2 * ALPHA * loss(net_x, target)
+size = 1
+for val in list(net_x.shape):
+	size = size * val
+net_x = net_x.resize(int(size/N_FILTERS), N_FILTERS)
+gram = np.matmul(net_x.data.numpy().T, net_x.data.numpy())/ N_SAMPLES
+style_loss = 2 * np.square(np.subtract(gram, style_gram)).mean()
 
+# style_loss = 2 * loss(gram, style_gram)
+loss = content_loss + style_loss
+print(loss)
 
 
