2c88164bbc21310175eca8cc34b6e3bf5c4730b2
Naveen Bansal <naveen18jan93@gmail.com>
2018-04-01 21:07:06 -0400

    checking in working code for neural style transfer vanilla approach in pytorch

--- neural-style-audio-pt.py
+++ neural-style-audio-pt.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import librosa
 import os
+from torch import optim
 import numpy as np
 from torch.autograd import Variable
 from sys import stderr
@@ -11,6 +12,11 @@ STYLE_FILENAME = "/home/naveen/neural-style-audio-tf/inputs/gajiniInstrumental.m
 
 N_FFT = 2048
 
+def get_input_param_optimizer(input_val):
+    # this line to show that input is a parameter that requires a gradient
+    input_param = nn.Parameter(input_val.data)
+    optimizer = optim.LBFGS([input_param])
+    return input_param, optimizer
 
 def read_audio_spectum(filename):
     x, fs = librosa.load(filename)
@@ -56,14 +62,15 @@ a_style = a_style[:N_CHANNELS, :N_SAMPLES]
 
 N_FILTERS = 4096
 
-a_content_tf = np.ascontiguousarray(a_content.T[None,None,:,:])
+content_copy = np.copy(a_content)
+a_content_tf = np.ascontiguousarray(content_copy.T[None,None,:,:])
 a_style_tf = np.ascontiguousarray(a_style.T[None,None,:,:])
 
 a_content_tf = np.transpose(a_content_tf, (0, 3, 1, 2))
 a_style_tf = np.transpose(a_style_tf, (0, 3, 1, 2))
 
-std = np.sqrt(2) * np.sqrt(2.0 / ((N_CHANNELS + N_FILTERS) * 11))
-kernel = np.random.randn(1, 11, N_CHANNELS, N_FILTERS)*std
+# std = np.sqrt(2) * np.sqrt(2.0 / ((N_CHANNELS + N_FILTERS) * 11))
+# kernel = np.random.randn(1, 11, N_CHANNELS, N_FILTERS)*std
 
 content_var = Variable(torch.from_numpy(a_content_tf), requires_grad = False)
 style_var = Variable(torch.from_numpy(a_style_tf), requires_grad = False)
@@ -74,10 +81,11 @@ model = torch.nn.Sequential(
 		)
 
 content_features = model(content_var)
-style_features = model(style_var)
+style_features = model(style_var).clone()
 
 style_features = style_features.view(-1, N_FILTERS)
-# style_gram = torch.mm(style_features.transpose(0,1), style_features)/N_SAMPLES
+gram = GramMatrix()
+style_gram_target = gram(style_features)
 
 ALPHA = 1e-2
 learning_rate = 1e-2
@@ -85,45 +93,21 @@ iterations = 100
 result = None
 
 # x = Variable(torch.randn(1, N_CHANNELS, 1, N_SAMPLES).type(torch.FloatTensor)*1e-3, requires_grad = True)
-x = torch.nn.Parameter(torch.from_numpy(a_content_tf), requires_grad = True)
-
-loss_fn = torch.nn.MSELoss()
-style_loss = 0
-
-optimizer = torch.optim.LBFGS([x], lr=learning_rate)
-gram = GramMatrix()
-style_gram_target = gram(style_features)
-# style_gram_target = Variable(style_gram.data, requires_grad = False)
-
-
-# def closure_version1():
-# 	global x
-# 	optimizer.zero_grad()
-# 	output = model(x)
-# 	net_x = output.view(-1, N_FILTERS)
-# 	gram = Variable((torch.mm(net_x.transpose(0,1), net_x).data/ N_SAMPLES), requires_grad = True)
-# 	loss = StyleLoss(gram, style_gram_target)
-# 	print(t, loss.data[0])
-# 	gr = torch.autograd.grad([loss], [x])
-# 	x = x + learning_rate * gr[0]
-# 	loss.backward()
-# 	return loss
+x, optimizer = get_input_param_optimizer(content_var)
 
 gram = GramMatrix()
-style_loss_module = StyleLoss(style_gram_target, 1000)
+style_loss_module = StyleLoss(style_gram_target, 300)
 model.add_module("style_loss", style_loss_module)
 
-for t in range(10):
-	print(t)
+t = [0]
+while t[0] < 100:
 	def closure_version2():
 		style_score = 0
 		optimizer.zero_grad()
-		# net_x = model(x)
-		# net_x = net_x.view(-1, N_FILTERS)
-		# gram = Variable((torch.mm(net_x.transpose(0,1), net_x).data/ N_SAMPLES), requires_grad = True)
 		model(x)
-		# print(t, loss.data[0])
+		t[0] += 1
 		style_score += style_loss_module.backward()
+		print(t[0], style_score.data[0])
 		return style_score
 	optimizer.step(closure_version2)
 
@@ -141,16 +125,16 @@ print("x = ", x)
 diff = a_content - x.T
 print("diff = ", diff)
 
-# a = np.zeros_like(a_content)
-# a[:N_CHANNELS,:] = np.exp(x.T) - 1
-# p = 2 * np.pi * np.random.random_sample(a.shape) - np.pi
-# for i in range(500):
-#     S = a * np.exp(1j*p)
-#     x = librosa.istft(S)
-#     p = np.angle(librosa.stft(x, N_FFT))
+a = np.zeros_like(a_content)
+a[:N_CHANNELS,:] = np.exp(x.T) - 1
+p = 2 * np.pi * np.random.random_sample(a.shape) - np.pi
+for i in range(500):
+    S = a * np.exp(1j*p)
+    x = librosa.istft(S)
+    p = np.angle(librosa.stft(x, N_FFT))
 
-# OUTPUT_FILENAME = 'outputs/out.wav'
-# librosa.output.write_wav(OUTPUT_FILENAME, x, fs)
-# print(OUTPUT_FILENAME)
-# print("done")
+OUTPUT_FILENAME = 'outputs/out.wav'
+librosa.output.write_wav(OUTPUT_FILENAME, x, fs)
+print(OUTPUT_FILENAME)
+print("done")
 
