277aa94ceb751ec2fea9bdba4629fe0bf25e9bcc
Naveen Bansal <naveen18jan93@gmail.com>
2018-04-03 20:06:19 -0400

    Tried the other version with both content and style loss

--- neural-style-audio-pt.py
+++ neural-style-audio-pt.py
@@ -7,14 +7,14 @@ import numpy as np
 from torch.autograd import Variable
 from sys import stderr
 
-CONTENT_FILENAME = "/home/naveen/neural-style-audio-tf/inputs/gajiniLyrical.mp3"
-STYLE_FILENAME = "/home/naveen/neural-style-audio-tf/inputs/gajiniInstrumental.mp3"
+CONTENT_FILENAME = "/home/naveen/neural-style-audio-tf/inputs/gettysburg.mp3"
+STYLE_FILENAME = "/home/naveen/neural-style-audio-tf/inputs/eminem.mp3"
 
 N_FFT = 2048
 
 def get_input_param_optimizer(input_val):
     # this line to show that input is a parameter that requires a gradient
-    input_param = nn.Parameter(input_val.data)
+    input_param = nn.Parameter(input_val.clone().data)
     optimizer = optim.LBFGS([input_param])
     return input_param, optimizer
 
@@ -31,6 +31,27 @@ class GramMatrix(nn.Module):
 		features = input.view(-1, N_FILTERS)
 		G = torch.mm(features.transpose(0,1), features)
 		return G.div(N_SAMPLES)
+
+class ContentLoss(nn.Module):
+
+    def __init__(self, target, weight):
+        super(ContentLoss, self).__init__()
+        # we 'detach' the target content from the tree used
+        self.target = target.detach() * weight
+        # to dynamically compute the gradient: this is a stated value,
+        # not a variable. Otherwise the forward method of the criterion
+        # will throw an error.
+        self.weight = weight
+        self.criterion = nn.MSELoss()
+
+    def forward(self, input):
+        self.loss = self.criterion(input * self.weight, self.target)
+        self.output = input
+        return self.output
+
+    def backward(self, retain_graph=True):
+        self.loss.backward(retain_graph=retain_graph)
+        return self.loss
 
 class StyleLoss(nn.Module):
 
@@ -62,10 +83,13 @@ a_style = a_style[:N_CHANNELS, :N_SAMPLES]
 
 N_FILTERS = 4096
 
+# changing x changes the a_content for some reason, so using deepcopy
 content_copy = np.copy(a_content)
+
 a_content_tf = np.ascontiguousarray(content_copy.T[None,None,:,:])
 a_style_tf = np.ascontiguousarray(a_style.T[None,None,:,:])
 
+#rearrange axis to meet conv2d input format requirement
 a_content_tf = np.transpose(a_content_tf, (0, 3, 1, 2))
 a_style_tf = np.transpose(a_style_tf, (0, 3, 1, 2))
 
@@ -93,25 +117,34 @@ iterations = 100
 result = None
 
 # x = Variable(torch.randn(1, N_CHANNELS, 1, N_SAMPLES).type(torch.FloatTensor)*1e-3, requires_grad = True)
+rand_input = Variable(torch.randn(1, N_CHANNELS, 1, N_SAMPLES).type(torch.FloatTensor)*1e-3, requires_grad = True)
+# x, optimizer = get_input_param_optimizer(rand_input)
 x, optimizer = get_input_param_optimizer(content_var)
 
 gram = GramMatrix()
 style_loss_module = StyleLoss(style_gram_target, 300)
+content_loss_module = ContentLoss(content_features.clone(), 10)
 model.add_module("style_loss", style_loss_module)
+# model.add_module("content_loss", content_loss_module)
+
 
 t = [0]
 while t[0] < 100:
 	def closure_version2():
-		style_score = 0
+		score = 0
 		optimizer.zero_grad()
 		model(x)
 		t[0] += 1
-		style_score += style_loss_module.backward()
-		print(t[0], style_score.data[0])
-		return style_score
+		# total_loss = style_loss_module.backward() + content_loss_module.backward()
+		total_loss = style_loss_module.backward()
+		score += total_loss
+		print(t[0], score.data[0])
+		return score
 	optimizer.step(closure_version2)
 
 x = x.data.numpy()
+
+#undo the axis remarrangement done before
 x = np.transpose(x, (0, 2, 3, 1))
 x = np.reshape(x, (-1, N_CHANNELS))
 
@@ -133,7 +166,7 @@ for i in range(500):
     x = librosa.istft(S)
     p = np.angle(librosa.stft(x, N_FFT))
 
-OUTPUT_FILENAME = 'outputs/out.wav'
+OUTPUT_FILENAME = 'outputs/eminem_gettysberg.wav'
 librosa.output.write_wav(OUTPUT_FILENAME, x, fs)
 print(OUTPUT_FILENAME)
 print("done")
